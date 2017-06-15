0ec72a5be19315c5bc7b5393b0072dccbbd5e617
Borja <dborja.dam@gmail.com>
2017-06-15 08:37:00 +0200

    Bloques con disño grafico

--- BloqueCobertura.java
+++ BloqueCobertura.java
@@ -1,16 +1,13 @@
 import javafx.scene.shape.Rectangle;
 import java.util.ArrayList;
-/**
- * Write a description of class BloqueCobertura here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 public class BloqueCobertura extends Rectangle
 {
     private static final int ANCHO_BC = 40;
     private static final int ALTO_BC = 30;
     private int puntosVida;
+    protected Image imagenBloque;
 
     public BloqueCobertura (int valorX){
         setWidth(ANCHO_BC);
@@ -18,6 +15,8 @@ public class BloqueCobertura extends Rectangle
         setTranslateX(valorX);
         setTranslateY(395);
         puntosVida=10;
+        imagenBloque=new Image("Imagenes/bloque.png");
+        setFill( new ImagePattern(imagenBloque));
     }
     
     public int getVida(){
